fbf513046cf5e8ac3e85fad9b0e61eeedb4b628f
parth <parth305pandya@gmail.com>
2022-10-15 12:12:20 +0530

    atlas connected

--- hrms-server-api/server/connector/connectMongo.js
+++ hrms-server-api/server/connector/connectMongo.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 const connectMongo = () => {
-    mongoose.connect('mongodb://localhost:27017/demo')
+    mongoose.connect('mongodb+srv://HRMS:HRMS@cluster0.yytneuy.mongodb.net/?retryWrites=true&w=majority')
+    // mongoose.connect('mongodb://localhost:27017/demo')
         .then(() => {
             console.log("Connected successfully")
         })
