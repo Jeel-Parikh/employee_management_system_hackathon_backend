7ce933c924392546cde318750057406b10d368ed
Jeel Parikh <jeel1512002@gmail.com>
2022-10-16 00:34:19 +0530

    [Leave working]

--- hrms-server-api/server/routes/leave/controller.js
+++ hrms-server-api/server/routes/leave/controller.js
@@ -3,10 +3,11 @@ import getCurrentDate from "../../services/date"
 
 const addLeaveById = ((req, res) => {
     // const { startDate, endDate,reason,title,admin_response,type } = req.body
-    let reqObj=req.body
-    reqObj.userId=req.params.id
-    Leave.create({ userId: req.params.id, appliedDate: getCurrentDate() })
-
+    let reqObj = req.body
+    reqObj.userId = req.params.id
+    reqObj.appliedDate = getCurrentDate()
+    // console.log("===============", reqObj)
+    Leave.create(reqObj)
         .then((resul) => {
 
             Leave.findOne({ "_id": resul._id }).populate("userId")
@@ -27,19 +28,20 @@ const showLeaves = ((req, res) => {
         .then((result) => {
             res.json({ response: true, result: result });
         })
-        .catch(err => console.log("error in addUserDetail", err));
+        .catch(err => console.log("error in leave", err));
 })
 
 const updateLeaveStatus = ((req, res) => {
+    console.log("===============", req.body.admin_response)
 
     // Leave.findOneAndUpdate({ userId: req.params.id, date: new Date(req.params.date), "Leave._id": req.body._id }, { "Leave.$.status": req.body.status }, { runValidators: true, new: true }).populate("userId")
-    Leave.findOneAndUpdate({ "Leave._id": req.params.id }, { "Leave.$.status": req.body.status }, { runValidators: true, new: true }).populate("userId")
+    Leave.findOneAndUpdate(req.params.id, { admin_response: req.body.admin_response }, { runValidators: true, new: true }).populate("userId")
         .then((result) => {
             // console.log("------->result", result)
             // return
             res.json({ response: true, result: result });
         })
-        .catch(err => console.log("error in addUserDetail", err));
+        .catch(err => console.log("error in leave", err));
 })
 
 
